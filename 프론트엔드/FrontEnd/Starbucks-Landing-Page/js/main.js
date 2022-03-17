60683fe37a30c60482ef0da646614bdd8b84b468
Moon-gd <king4mun@gmail.com>
2022-03-17 14:11:53 +0900

    Starbucks dropdown 완성, visual-section 만드는 중 1

--- 프론트엔드/FrontEnd/Starbucks-Landing-Page/js/main.js
+++ 프론트엔드/FrontEnd/Starbucks-Landing-Page/js/main.js
@@ -18,3 +18,26 @@ searchElInput.addEventListener('blur', function() {
   searchEl.classList.remove('focused');
   searchElInput.setAttribute('placeholder', '');
 })
+
+const badgeEl = document.querySelector('header .badges');
+window.addEventListener('scroll', _.throttle(function() {
+  /*
+    throttle ? 
+      lodash cdn을 통해 불러온 함수로써 _.throttle(콜백 함수, 지연 시간<밀리세컨드 단위>) 와 같이 사용할 수 있다.
+   */
+  if(window.scrollY > 500) {
+    /*
+      gsap.to(요소, 지속시간(s), 옵션)
+     */
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display:'none'
+    });
+  }
+  else {
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
