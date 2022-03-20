d24827c4c4de29dbb6ace88c0b404c20444751c4
Moon-gd <king4mun@gmail.com>
2022-03-20 16:34:20 +0900

    Starbucks 요소 슬라이드 완성

--- 프론트엔드/FrontEnd/Starbucks-Landing-Page/js/main.js
+++ 프론트엔드/FrontEnd/Starbucks-Landing-Page/js/main.js
@@ -56,3 +56,38 @@ new Swiper('.notice-line .swiper-container', {
   autoplay:true,
   loop:true
 })
+
+new Swiper('.promotion .swiper-container', {
+  direction: 'horizontal',
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides:true,
+  loop:true,
+  // autoplay: {
+  //   delay:500
+  // }
+  pagination: {
+    el:'.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable:true,  // 사용자의 페이지 제어 가능 여부
+  },
+  navigation: {
+      prevEl: '.promotion .swiper-prev',
+      nextEl: '.promotion .swiper-next'
+  }
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener('click', function() {
+  isHidePromotion = !isHidePromotion;
+  if(isHidePromotion) {
+    // 숨김 처리
+    promotionEl.classList.add('hide');
+  }
+  else {
+    // 보임 처리
+    promotionEl.classList.remove('hide');
+  }
+});
